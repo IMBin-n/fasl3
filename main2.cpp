3457480ee6d2c6bce5e19939d35f6b2dd1dfbef7
IMBin-n <115714588+IMBin-n@users.noreply.github.com>
2022-12-05 18:16:14 +0330

    Add files via upload

---
+++ main2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std ;
+int main() {
+    string a,b,c,d;
+    std::cout << "Hello, enter 4 words:" << std::endl;
+    cin>> a >> b >> c >> d;
+    cout <<a<<" * "<<b<<" * "<<c<<" * "<<d<<endl ;
+    cout <<d<<" * "<<c<<" * "<<b<<" * "<<a<<endl ;
+
+    return 0;
+}
